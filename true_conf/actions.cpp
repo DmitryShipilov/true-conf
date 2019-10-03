562a6ed942446cdf1832b55ea154a6158d4d47d5
Dmitry <d-shipilov@mail.ru>
2019-10-03 14:08:13 +0300

    Make file

--- true_conf/actions.cpp
+++ true_conf/actions.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "actions.h"
 
 void FillVectorAndMap(vector<int>& v, map<int, int>& m)
--- true_conf/actions.h
+++ true_conf/actions.h
@@ -1,4 +1,16 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include <ctime>
+
+using namespace std;
+
+const uint8_t lowBorder = 1;																// constants for range [1, 9]
+const uint8_t highBorder = 10;
+const uint8_t minNumberOfElements = 15;
+
+
 int IntNumberFromZeroToFifteen(void);													// returns random value in range [1, 15]
 
 void FillVectorAndMap(vector<int>& v, map<int, int>& m);								// fills vector and map with random values
